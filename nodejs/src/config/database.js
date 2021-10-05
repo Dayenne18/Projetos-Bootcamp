204299ef0c4c7d70115d987ef709a0b0007c44e4
Denis Moreira <91612514+Dayenne18@users.noreply.github.com>
2021-10-05 19:26:27 -0300

    Deletando Projeto
    Erro conta ao enviar

--- nodejs/src/config/database.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql');
-
-module.exports = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  port: 3307,
-  database: 'fseletro',
-});
--- projeto-bootstrap/conexao.php
+++
@@ -1,7 +0,0 @@
-<?php 
-  $connection = mysqli_connect('localhost', 'root', '', 'fseletro');
-
-  if (!$connection) {
-    die ("A conexão não foi realizada" . mysqli_connect_error());
-  }
-?>
